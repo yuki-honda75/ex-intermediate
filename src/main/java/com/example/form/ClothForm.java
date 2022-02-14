990b65c29e2780bba3b8a8faefb6b8bfa4afb713
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-14 18:49:21 +0900

    create ClothForm

---
+++ src/main/java/com/example/form/ClothForm.java
@@ -0,0 +1,27 @@
+package com.example.form;
+
+/**
+ * 
+ * @author hondayuki
+ *
+ */
+public class ClothForm {
+	/** カラー */
+	private String color;
+	/** 性別 */
+	private Integer gender;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public Integer getGender() {
+		return gender;
+	}
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	
+}
