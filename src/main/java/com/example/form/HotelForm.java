ca8cb40b64707b675c83e462c42cce4be222df16
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-14 16:38:52 +0900

    fix

--- src/main/java/com/example/form/HotelForm.java
+++ src/main/java/com/example/form/HotelForm.java
@@ -12,7 +12,7 @@ import org.hibernate.validator.constraints.Range;
 public class HotelForm {
 	/** 価格 */
 	@Range(min = 1, max = 300000, message = "価格は300,000までの値で入力してください")
-	@Pattern(regexp = "^[0-9]+$", message = "数字を入力してください")
+	@Pattern(regexp = "^[0-9]+$", message = "正の数字を入力してください")
 	private String price;
 
 	public String getPrice() {
