720e81fca6ea429a5226b07dcc8a0f0765ca775b
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-14 10:35:31 +0900

    create Team.java for domain

---
+++ src/main/java/com/example/domain/Team.java
@@ -0,0 +1,50 @@
+package com.example.domain;
+
+/**
+ * 
+ * @author hondayuki
+ *
+ */
+public class Team {
+	/** 球団名 */
+	private String teamName;
+	/** 本拠地 */
+	private String headquarters;
+	/** 発足 */
+	private String inauration;
+	/** 歴史 */
+	private String history;
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getHeadquarters() {
+		return headquarters;
+	}
+	public void setHeadquarters(String headquarters) {
+		this.headquarters = headquarters;
+	}
+	public String getInauration() {
+		return inauration;
+	}
+	public void setInauration(String inauration) {
+		this.inauration = inauration;
+	}
+	public String getHistory() {
+		return history;
+	}
+	public void setHistory(String history) {
+		this.history = history;
+	}
+	
+	@Override
+	public String toString() {
+		return "Team [teamName=" + teamName
+				+ ", headquarters=" + headquarters
+				+ ", inauration=" + inauration
+				+ ", history=" + history + "]";
+	}
+}
